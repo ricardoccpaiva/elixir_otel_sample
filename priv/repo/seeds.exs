4c9415c0764e8e585d4d361c8f596608746d919f
Ricardo Paiva <ricardoccpaiva@gmail.com>
2022-05-07 14:01:26 +0100

    Add seed data

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,7 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+names = ~w(molly bob alice peter susan richard doug ryan mary rob)
+
+Enum.each(names, fn name -> ElixirOtelSample.Repo.insert!(%ElixirOtelSample.Accounts.User{name: name}) end)
